c50db2d4e3a45b9fbc8681070803109ef199b595
Miyamoto Musashi <miyamoto@Miyamotos-Air.fritz.box>
2022-05-23 14:22:15 -0400

    Added my experiment branch

--- PoseDetection.py
+++ PoseDetection.py
@@ -18,12 +18,12 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    # print(results.pose_landmarks)
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        print('LANDMARKS ARE',results.pose_landmarks)
         for id, lm in enumerate(results.pose_landmarks.landmark):
             h, w, c = img.shape
-            print(id, lm)
+            # print(id, lm)
             cx, cy = int(lm.x * w), int(lm.y * h)
             cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
